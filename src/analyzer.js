01952c5a2c6457add622126307750b94354ac9b2
Pamela Olguin <pame.ertek@gmail.com>
2024-05-27 10:31:25 -0400

    realizado el método analyzer de contador de caracteres y caracteres sin espacios

--- src/analyzer.js
+++ src/analyzer.js
@@ -21,6 +21,20 @@ return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+ 
+ 
+    let charNoSpace= 0;
+
+    for(let c of text)
+      {
+          if( c!=' ' ){
+              charNoSpace++;
+          }
+      }
+
+      return charNoSpace;
+ 
+ 
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ const liWordCount = document.querySelector('li[data-testid="word-count"]');
 
 const liCharCount = document.querySelector('li[data-testid="character-count"]');
 
+const liCharNoSpacesCount = document.querySelector('li[data-testid="character-no-spaces-count"]');
+
+
+
 textBox.addEventListener('input',function(){
 
     liWordCount.textContent='Palabras :'+analyzer.getWordCount(this.value);
 
     liCharCount.textContent='Caracteres :'+analyzer.getCharacterCount(this.value);
+    
+    liCharNoSpacesCount.textContent='Caracteres sin espacios :'+analyzer.getCharacterCountExcludingSpaces(this.value);
+
 
 });
 

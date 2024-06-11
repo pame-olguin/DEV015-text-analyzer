6be3186cb7f5cf73e8cefab2b02429f110725b71
Pamela Olguin <pame.ertek@gmail.com>
2024-06-11 16:11:54 -0400

    metodo caracteres sin espacio mejorado, tests ok

--- src/analyzer.js
+++ src/analyzer.js
@@ -22,10 +22,11 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
  
     let charNoSpace= 0;
+    const punct = [' ','.',',','!',':',';','¡','{','}','[',']','(',')','/','?','¿','#','"',"'",'&','%','=','$','*','-','+','~','`','<','>'];
 
     for(const c of text)
     {
-      if( c!==' ' && c!==',' && c!=='.'){
+      if( !punct.includes(c) ){
         charNoSpace++;
       }
     }
